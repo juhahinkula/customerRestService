560bdaa238ed4103545981bd8558b60f91563d5c
juhahinkula <hinkula@gmail.com>
2018-01-10 18:26:35 +0200

    demo data

--- src/main/java/fi/haagahelia/customer/CustomerApplication.java
+++ src/main/java/fi/haagahelia/customer/CustomerApplication.java
@@ -24,12 +24,37 @@ public class CustomerApplication {
 		return (args) -> {
 			Customer c = new Customer("John", "Johnson", "5th Street", "23110", "Flintsone", "john@mail.com", "232-2345540");
 			repository.save(c);
-			repository.save(new Customer("Mary", "Philips", "Hill Street 3", "23322", "Flintsone", "m.philips@mail.com", "232-310122"));
-			repository.save(new Customer("Dan", "Davidson", "Main Road 32 B", "23130", "Flintsone", "dan.d@mail.com", "232-1227006"));
+			Customer c2 = new Customer("Mary", "Philips", "Hill Street 3", "23322", "Flintsone", "m.philips@mail.com", "232-310122");
+			repository.save(c2);
+			Customer c3 = new Customer("Dan", "Davidson", "Main Road 32 B", "23130", "Flintsone", "dan.d@mail.com", "232-1227006");
+			repository.save(c3);
 		
 			Training t = new Training(new Date(), 60, "Spinning");
 			t.setCustomer(c);
 			trepository.save(t);
+			t = new Training(new Date(), 30, "Gym training");
+			t.setCustomer(c);
+			trepository.save(t);
+			
+			t = new Training(new Date(), 90, "Gym training");
+			t.setCustomer(c2);
+			trepository.save(t);
+			t = new Training(new Date(), 60, "Fitness");
+			t.setCustomer(c2);
+			trepository.save(t);
+			t = new Training(new Date(), 60, "Spinning");
+			t.setCustomer(c2);
+			trepository.save(t);
+
+			t = new Training(new Date(), 45, "Gym training");
+			t.setCustomer(c3);
+			trepository.save(t);
+			t = new Training(new Date(), 45, "Zumba");
+			t.setCustomer(c3);
+			trepository.save(t);
+			t = new Training(new Date(), 60, "Zumba");
+			t.setCustomer(c3);
+			trepository.save(t);			
 		};
 	}
 
